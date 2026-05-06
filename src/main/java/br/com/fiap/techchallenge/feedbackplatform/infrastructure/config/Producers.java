be2b019e6ecc5aff88d145b1bd533cd9f7311b13
Kervin Candido <kervincandido@gmail.com>
2026-05-05 21:46:02 -0300

    fix(infra): integra EmailSender no CreateFeedbackUseCase
    Corrige a configuração do bean CreateFeedbackUseCase injetando o EmailSender e adicionando-o à lista de notificadores.

--- src/main/java/br/com/fiap/techchallenge/feedbackplatform/infrastructure/config/Producers.java
+++ src/main/java/br/com/fiap/techchallenge/feedbackplatform/infrastructure/config/Producers.java
@@ -5,6 +5,7 @@ import java.util.List;
 import br.com.fiap.techchallenge.feedbackplatform.application.ports.FeedbackRepositoryPort;
 import br.com.fiap.techchallenge.feedbackplatform.application.usecase.CreateFeedbackUseCase;
 import br.com.fiap.techchallenge.feedbackplatform.infrastructure.classifier.FeedbackUrgenciaClassifierAdapter;
+import br.com.fiap.techchallenge.feedbackplatform.infrastructure.notify.EmailSender;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
@@ -13,8 +14,9 @@ public class Producers {
 
     @Produces
     @ApplicationScoped
-    public CreateFeedbackUseCase createFeedbackUseCaseProducer(FeedbackRepositoryPort feedbackRepository) {
+    public CreateFeedbackUseCase createFeedbackUseCaseProducer(
+            FeedbackRepositoryPort feedbackRepository, EmailSender emailSender) {
         return new CreateFeedbackUseCase(feedbackRepository, new FeedbackUrgenciaClassifierAdapter(),
-                List.of((feedback) -> System.out.println("Feedback urgente: " + feedback)));
+                List.of((feedback) -> emailSender.send(feedback)));
     }
 }
